7695eb9e3454f36242b5572986d9e24ff2fba1af
sandeshit <thapasandesh56@gmail.com>
2025-01-15 19:22:00 +0545

    Chapter three completed

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
-from django.http import HttpResponse
+class HomePageView(TemplateView):
+    template_name = 'home.html'
 
-def homePageView(request):
-    return HttpResponse('hello, World')
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
