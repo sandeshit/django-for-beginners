7f9178d59b344f9db80e7c2b527f65b90bd8abfd
sandeshit <thapasandesh56@gmail.com>
2025-02-07 16:08:45 +0545

    Chapter 6 finished(Forms)

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,7 @@
 from .models import BlogPost
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 class BlogListView(ListView):
     model = BlogPost
@@ -7,6 +9,21 @@ class BlogListView(ListView):
 
 # Create your views here.
 class BlogDetailView(DetailView):
-    model = BlogPost
+    model = BlogPost  
     template_name = 'post_detail.html'
 
+
+class BlogCreateView(CreateView):
+    model = BlogPost
+    template_name = 'post_new.html'
+    fields = ['title','author','body']
+
+class BlogUpdateView(UpdateView):
+    model = BlogPost
+    template_name = 'post_edit.html'
+    fields = ['title','body']
+
+class BlogDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('blogs_home')
